1f09b809e28d2bb7172577b260a1a1da97747c13
Reinhard Nägele <reinhard.naegele@mgm-tp.com>
2013-04-29 13:01:16 +0200

    Initial commit

---
+++ perfload-demo-driver/src/main/java/com/mgmtp/perfload/demo/driver/DemoDriverModule.java
@@ -0,0 +1,19 @@
+package com.mgmtp.perfload.demo.driver;
+
+import com.mgmtp.perfload.core.client.web.config.AbstractWebLtModule;
+import com.mgmtp.perfload.core.common.util.PropertiesMap;
+
+/**
+ * @author rnaegele
+ */
+public class DemoDriverModule extends AbstractWebLtModule {
+
+	public DemoDriverModule(final PropertiesMap testplanProperties) {
+		super(testplanProperties);
+	}
+
+	@Override
+	protected void doConfigureWebModule() {
+
+	}
+}
