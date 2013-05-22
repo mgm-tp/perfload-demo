4a0b1c35225dc0a75bc7842a01b6813c1f70751f
Julian Sievers <julian.sievers@mgm-tp.com>
2013-05-22 12:19:34 +0200

    removed unnecessary config
    Change-Id: Ic88b1aa332fc89be2a10ecc5f5132eff2f0025d8

--- perfload-demo-installer/src/main/scripts/demo-setup/supervisor/conf/LoadTestConfig.groovy
+++ perfload-demo-installer/src/main/scripts/demo-setup/supervisor/conf/LoadTestConfig.groovy
@@ -21,15 +21,3 @@ hostConfigs {
 		}
 	}
 }
-// Host Configurations
-
-hostConfigs {
-	localhost {
-		user = 'myuser'
-		password = 'mypass'
-		perfLoadHome = '..' // relative path
-		osfamily = 'windows' // or 'unix'
-		client = true
-		perfmon = true
-	}
-}
